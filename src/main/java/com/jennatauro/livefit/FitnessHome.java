921047f52014d9cfbd36aafc0b5af371803bc192
Jenna Tauro <jennatauro@gmail.com>
2014-02-22 14:11:44 -0500

    Added assets, refactored home screen view.

---
+++ src/main/java/com/jennatauro/livefit/FitnessHome.java
@@ -0,0 +1,29 @@
+package com.jennatauro.livefit;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+
+public class FitnessHome extends ActionBarActivity{
+
+    public static void start(Context context) {
+        final Intent intent = new Intent(context, FitnessHome.class);
+        context.startActivity(intent);
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fitness_home);
+    }
+}
