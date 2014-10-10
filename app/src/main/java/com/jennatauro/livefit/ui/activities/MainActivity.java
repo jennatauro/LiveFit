c22fc68486ade733de0393601b9096af0cec77ae
Jenna Tauro <jennatauro@gmail.com>
2014-10-09 21:44:47 -0400

    Added Resources from old live fit project

--- app/src/main/java/com/jennatauro/livefit/ui/activities/MainActivity.java
+++ app/src/main/java/com/jennatauro/livefit/ui/activities/MainActivity.java
@@ -21,47 +21,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        DbHelper helper = new DbHelper(this);
-        Workout workout = new Workout();
-        workout.setTitle("Created Workout");
-        workout.setDescription("Desc1");
-
-        List<Exercise> list = new ArrayList<Exercise>();
-        Exercise exercise = new Exercise();
-        exercise.setTitle("Exercise Title");
-        list.add(exercise);
-        workout.setExercises(list);
-
-        Workout workout1 = new Workout();
-        workout1.setTitle("Second Workout");
-        workout1.setDescription("Desc2");
-
-        helper.createOrUpdateWorkoutWithExercises(workout);
-        helper.createOrUpdateWorkoutWithExercises(workout1);
-
-
-        List<Workout> workouts = helper.getWorkouts();
-        ((TextView) findViewById(R.id.textview)).setText(workouts.get(0).getTitle());
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
