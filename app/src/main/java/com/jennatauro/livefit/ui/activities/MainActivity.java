27d502bf963f3ec9455d3e0dfafce1126d089887
Jenna Tauro <jennatauro@gmail.com>
2014-10-09 21:58:04 -0400

    Set on click listener for fitness button

--- app/src/main/java/com/jennatauro/livefit/ui/activities/MainActivity.java
+++ app/src/main/java/com/jennatauro/livefit/ui/activities/MainActivity.java
@@ -23,6 +23,8 @@ public class MainActivity extends Activity implements View.OnClickListener{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.ll_fitness).setOnClickListener(this);
     }
 
     @Override
