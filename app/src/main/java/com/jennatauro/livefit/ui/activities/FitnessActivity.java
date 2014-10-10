4e5ef5445994cac5ccadfff74c69e86cb470dbea
Jenna Tauro <jennatauro@gmail.com>
2014-10-09 21:57:09 -0400

    Added fitness activity with layout

---
+++ app/src/main/java/com/jennatauro/livefit/ui/activities/FitnessActivity.java
@@ -0,0 +1,18 @@
+package com.jennatauro.livefit.ui.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.jennatauro.livefit.R;
+
+/**
+ * Created by jennatauro on 2014-10-09.
+ */
+public class FitnessActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fitness);
+    }
+}
--- app/src/main/java/com/jennatauro/livefit/ui/activities/MainActivity.java
+++ app/src/main/java/com/jennatauro/livefit/ui/activities/MainActivity.java
@@ -1,9 +1,11 @@
 package com.jennatauro.livefit.ui.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 import com.jennatauro.livefit.R;
@@ -15,7 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,4 +25,13 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.ll_fitness:
+                Intent intent = new Intent(this, FitnessActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
 }
