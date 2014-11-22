c3aae5cd5ee272cb35163ae28202b0903f5f069c
Jenna Tauro <jennatauro@gmail.com>
2014-11-22 15:15:31 -0500

    Added on click for create workout button

--- app/src/main/java/com/jennatauro/livefit/ui/activities/AddWorkoutActivity.java
+++ app/src/main/java/com/jennatauro/livefit/ui/activities/AddWorkoutActivity.java
@@ -3,13 +3,18 @@ package com.jennatauro.livefit.ui.activities;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 import com.jennatauro.livefit.R;
 
 /**
  * Created by jennatauro on 2014-11-22.
  */
-public class AddWorkoutActivity extends LiveFitActivity {
+public class AddWorkoutActivity extends LiveFitActivity implements View.OnClickListener{
+
+    private EditText workoutNameEditText;
+    private EditText workoutDescriptionEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +33,14 @@ public class AddWorkoutActivity extends LiveFitActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         finish();
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case (R.id.create_workout_button): {
+                //create workout
+            }
+        }
     }
 }
