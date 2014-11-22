61bda6deb1e9373b7c4a225af38fca84758d9c52
Jenna Tauro <jennatauro@gmail.com>
2014-11-22 12:33:25 -0500

    Fixed appearance of main activity

--- app/src/main/java/com/jennatauro/livefit/ui/activities/MainActivity.java
+++ app/src/main/java/com/jennatauro/livefit/ui/activities/MainActivity.java
@@ -1,23 +1,12 @@
 package com.jennatauro.livefit.ui.activities;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
 
 import com.jennatauro.livefit.R;
-import com.jennatauro.livefit.data.db.DbHelper;
-import com.jennatauro.livefit.data.models.Exercise;
-import com.jennatauro.livefit.data.models.Workout;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity implements View.OnClickListener{
