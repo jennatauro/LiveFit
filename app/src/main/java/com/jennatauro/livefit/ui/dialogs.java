7afd4450d82bf8f7a158dc32bbd9a7c546d0c4d3
Jenna Tauro <jennatauro@gmail.com>
2014-11-22 18:23:57 -0500

    Created exercise dialog

--- app/src/main/java/com/jennatauro/livefit/ui/dialogs.java
+++
@@ -1,7 +0,0 @@
-package com.jennatauro.livefit.ui;
-
-/**
- * Created by jennatauro on 2014-11-22.
- */
-public class dialogs {
-}
---
+++ app/src/main/java/com/jennatauro/livefit/ui/dialogs/ExerciseDialog.java
@@ -0,0 +1,7 @@
+package com.jennatauro.livefit.ui.dialogs;
+
+/**
+ * Created by jennatauro on 2014-11-22.
+ */
+public class ExerciseDialog {
+}
