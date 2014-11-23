be452df931090bbb56254f662634212917d90973
Jenna Tauro <jennatauro@gmail.com>
2014-11-23 17:36:47 -0500

    Set workout description

--- app/src/main/java/com/jennatauro/livefit/ui/activities/WorkoutDetailsActivity.java
+++ app/src/main/java/com/jennatauro/livefit/ui/activities/WorkoutDetailsActivity.java
@@ -3,6 +3,7 @@ package com.jennatauro.livefit.ui.activities;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.jennatauro.livefit.R;
 import com.jennatauro.livefit.data.db.DbHelper;
@@ -26,6 +27,8 @@ public class WorkoutDetailsActivity extends LiveFitActivity {
     @InjectView(R.id.workout_details_toolbar)
     Toolbar mToolbar;
 
+    Workout mWorkout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_workout_details);
@@ -35,14 +38,16 @@ public class WorkoutDetailsActivity extends LiveFitActivity {
 
         setSupportActionBar(mToolbar);
         try {
-            Workout workout = mDbHelper.getWorkoutForId(workoutId);
-            getSupportActionBar().setTitle(workout.getTitle());
+            mWorkout = mDbHelper.getWorkoutForId(workoutId);
+            getSupportActionBar().setTitle(mWorkout.getTitle());
         } catch (SQLException e) {
             getSupportActionBar().setTitle(getString(R.string.app_name));
         }
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
+
+        ((TextView) findViewById(R.id.workout_description)).setText(mWorkout.getDescription());
 
     }
 
