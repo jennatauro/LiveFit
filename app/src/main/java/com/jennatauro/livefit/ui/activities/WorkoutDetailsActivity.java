49a2eef5a9bc124291d27cfb71b19240c3f93afe
Jenna Tauro <jennatauro@gmail.com>
2014-11-23 17:58:18 -0500

    Show exercises on workout detail page

--- app/src/main/java/com/jennatauro/livefit/ui/activities/WorkoutDetailsActivity.java
+++ app/src/main/java/com/jennatauro/livefit/ui/activities/WorkoutDetailsActivity.java
@@ -1,16 +1,22 @@
 package com.jennatauro.livefit.ui.activities;
 
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.jennatauro.livefit.R;
 import com.jennatauro.livefit.data.db.DbHelper;
+import com.jennatauro.livefit.data.models.Exercise;
 import com.jennatauro.livefit.data.models.Workout;
+import com.jennatauro.livefit.ui.adapters.ExerciseAdapter;
 import com.jennatauro.livefit.ui.fragments.SeeAllWorkoutsFragment;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -24,15 +30,28 @@ public class WorkoutDetailsActivity extends LiveFitActivity {
     @Inject
     DbHelper mDbHelper;
 
+    @Inject
+    ExerciseAdapter mAdapter;
+
     @InjectView(R.id.workout_details_toolbar)
     Toolbar mToolbar;
 
     Workout mWorkout;
 
+    private RecyclerView mRecyclerView;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private List<Exercise> mExercises = new ArrayList<Exercise>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_workout_details);
         super.onCreate(savedInstanceState);
+
+        RecyclerView mRecyclerView = (RecyclerView) findViewById(R.id.exercises_recyclerview);
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
 
         int workoutId = getIntent().getIntExtra(SeeAllWorkoutsFragment.WORKOUT_ID, 0);
 
@@ -49,6 +68,12 @@ public class WorkoutDetailsActivity extends LiveFitActivity {
 
         ((TextView) findViewById(R.id.workout_description)).setText(mWorkout.getDescription());
 
+        displayExercises();
+    }
+
+    private void displayExercises() {
+        mExercises = mDbHelper.getExercisesForWorkout(mWorkout);
+        mAdapter.replace(mExercises);
     }
 
     @Override
