4012948b760f440825e0e0fd2d2f14cf22e6341d
Jenna Tauro <jennatauro@gmail.com>
2015-01-04 14:18:42 -0500

    Show all workouts in dialog

--- app/src/main/java/com/jennatauro/livefit/ui/fragments/AllWorkoutsFragmentDialog.java
+++ app/src/main/java/com/jennatauro/livefit/ui/fragments/AllWorkoutsFragmentDialog.java
@@ -5,13 +5,24 @@ import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.LinearLayout;
 
 import com.jennatauro.livefit.LivefitApplication;
 import com.jennatauro.livefit.R;
+import com.jennatauro.livefit.data.db.DbHelper;
+import com.jennatauro.livefit.data.models.Workout;
+import com.jennatauro.livefit.ui.adapters.WorkoutAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -22,6 +33,15 @@ import butterknife.InjectView;
 public class AllWorkoutsFragmentDialog extends DialogFragment {
 
     public static final String FRAGMENT_TAG = "com.jenatauro.livefit.AllWorkoutsFragmentDialog";
+
+    @Inject
+    WorkoutAdapter mAdapter;
+
+    private DbHelper mDbHelper;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private WorkoutsViewHolder mViewHolder;
+
+    private List<Workout> mWorkouts;
 
     public static AllWorkoutsFragmentDialog newInstance() {
         return new AllWorkoutsFragmentDialog();
@@ -36,7 +56,31 @@ public class AllWorkoutsFragmentDialog extends DialogFragment {
         LivefitApplication app = LivefitApplication.getApplication();
         app.inject(this);
 
+        mViewHolder = new WorkoutsViewHolder(view);
+
+        mDbHelper = new DbHelper(getActivity());
+
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mViewHolder.mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mViewHolder.mRecyclerView.setAdapter(mAdapter);
+
+        loadWorkouts();
+
         return view;
+    }
+
+    private void loadWorkouts() {
+        mWorkouts = mDbHelper.getWorkouts();
+        if (mWorkouts == null || mWorkouts.size() == 0) {
+            mViewHolder.noWorkoutsLayout.setVisibility(View.VISIBLE);
+            mViewHolder.mRecyclerView.setVisibility(View.GONE);
+            mAdapter.replace(new ArrayList<Workout>());
+        } else {
+            mViewHolder.noWorkoutsLayout.setVisibility(View.GONE);
+            mViewHolder.mRecyclerView.setVisibility(View.VISIBLE);
+            mAdapter.replace(mWorkouts);
+        }
     }
 
     public static void displayAllWorkoutsDialog(FragmentManager fm) {
@@ -57,6 +101,19 @@ public class AllWorkoutsFragmentDialog extends DialogFragment {
         super.onStart();
         if(getDialog() != null) {
             getDialog().getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        }
+    }
+
+    static class WorkoutsViewHolder {
+
+        @InjectView(R.id.all_workouts_recyclerview)
+        RecyclerView mRecyclerView;
+
+        @InjectView(R.id.no_workouts_yet_layout)
+        LinearLayout noWorkoutsLayout;
+
+        WorkoutsViewHolder(View view) {
+            ButterKnife.inject(this, view);
         }
     }
 
